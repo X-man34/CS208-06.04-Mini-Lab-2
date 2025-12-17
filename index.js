4e2471564b32bf671d47b4add7446876760778d0
Shane K. Panter <shane@foundationcode.com>
2025-12-16 18:35:39 -0700

    Add printSum function with test cases
    Initial implementation of printSum function with test cases.

---
+++ index.js
@@ -0,0 +1,12 @@
+function printSum(x, y) {
+   // TODO: Add your solution here
+   
+}
+
+console.log("Testing printSum()...");
+
+printSum(3, 6);            // 9
+printSum(3.5, 6.1);        // 9.6
+printSum("hello", 6);      // 'hello' is not a number
+printSum(10, "hi");        // 'hi' is not a number
+printSum("hello", "hi");   // 'hello' and 'hi' are not numbers
