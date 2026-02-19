cda89acde40fd21f17571e78552ca2680a334f16
X-man34 <caleb.hottes@gmail.com>
2026-02-19 13:43:41 -0700

    done

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function printSum(x, y) {
+  const xNum = parseFloat(x);
+  const yNum = parseFloat(y);
+  const xInvalid = isNaN(xNum);
+  const yInvalid = isNaN(yNum);
 
+  if (xInvalid && yInvalid) {
+    console.log(`'${x}' and '${y}' are not numbers.`);
+    return;
+  }
 
+  if (xInvalid) {
+    console.log(`'${x}' is not a number.`);
+    return;
+  }
+
+  if (yInvalid) {
+    console.log(`'${y}' is not a number.`);
+    return;
+  }
+
+  console.log(`Sum is ${xNum + yNum}.`);
 }
 
 export default printSum;
